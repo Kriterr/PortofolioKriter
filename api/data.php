da380f80d72a2a1761b2a8c7903b654306f4b6c1
Kriterr <christoidp86@gmail.com>
2025-09-21 16:57:40 +0700

    Add files via upload

---
+++ api/data.php
@@ -0,0 +1,8 @@
+<?php
+// Data informasi website (Edit sesuai kebutuhan Anda)
+$profileImage = "assets/profile.jpg"; // Ganti dengan path foto profile Anda
+$name = "Kriterr";
+$about = "Saya adalah seorang web developer yang berfokus pada pembuatan website modern, minimalis, dan efisien. Saya suka mengembangkan solusi digital yang mudah digunakan.";
+$github = "https://github.com/Kriterr";
+$linkedin = "https://linkedin.com/in/Kriterr";
+?>
