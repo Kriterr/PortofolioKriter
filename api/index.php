4f0962c40bd86b3f109431862e1bfef4018a86f9
Kriterr <christoidp86@gmail.com>
2025-09-23 20:23:12 +0700

    s

--- api/index.php
+++ api/index.php
@@ -11,6 +11,8 @@ require_once 'data.php';
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png"> ```
 </head>
 <body>
 
