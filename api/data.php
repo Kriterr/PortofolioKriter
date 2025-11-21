db758fa962786cf2593069ef5d536a3fc23695aa
Kriterr <christoidp86@gmail.com>
2025-11-21 16:24:58 +0700

    s

--- api/data.php
+++ api/data.php
@@ -56,7 +56,7 @@ $projects = [
     [
         'title' => 'KingsHcut Barbershop Reservation System',
         'description' => 'KingsHcut is a full-featured reservation system for a barber/salon, providing validated user registration, detailed service booking with date/time selection, and a dedicated admin dashboard for managing services and reservations.',
-        'link' => 'https://drive.google.com/drive/folders/1cXdtAPpDejxSCT4YOYS2g9yCTd5JaG0Y?usp=drive_link', // Replace with your actual link
+        'link' => 'https://drive.google.com/drive/folders/1cXdtAPpDejxSCT4YOYS2g9yCTd5JaG0Y?usp=sharing', // Replace with your actual link
         'image' => 'https://cdn.momoi.cc/uploads/2025-11-19_61806913cf/2025-11-19_a7509bea24.png'
     ],
     [
